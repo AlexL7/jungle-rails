fd26ec1af90d2ac36d0884f8996ec49d1bfd621a
Alexander <alexander.lock7@gmail.com>
2016-11-10 18:58:21 +0000

    added registration validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+
 end
