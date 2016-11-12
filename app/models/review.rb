72fe8d2cb53bd7f8e0c57d97a91ad35e6c9cfe49
Alexander <alexander.lock7@gmail.com>
2016-11-12 20:04:31 +0000

    added validations to the review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,11 @@ class Review < ActiveRecord::Base
 
   belongs_to :product
 
+
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
+  validates :rating, presence: true, length: { in: 1..5 }
+
+
 end
