756aa2481bec433e5ffa64ec56440569a9106186
Alexander <alexander.lock7@gmail.com>
2016-11-15 16:31:46 +0000

    current user session fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,9 @@ class ApplicationController < ActionController::Base
 
 
  def current_user
-  if (session[:user_id]= 2)
-    puts "no current user"
-  else
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
+    @current_user ||= User.find_by(id:session[:user_id]) if session[:user_id]
  end
+
  helper_method :current_user
 
   def authorize
